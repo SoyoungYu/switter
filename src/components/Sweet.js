05ec3ca7d3fb3c53f92ca2f7185b24bc434d9a6b
SoyoungYu <gsgtz_20@naver.com>
2021-09-30 16:03:37 +0900

    210930 update

---
+++ src/components/Sweet.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { dbService } from "fBase";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
+
+const Sweet = ({sweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newSweet, setNewSweet] = useState(sweetObj.text);
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this sweet?");
+        if (ok) {
+            await deleteDoc(doc(dbService, "sweets", sweetObj.id));
+        }
+    };
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await updateDoc(doc(dbService, `sweets/${sweetObj.id}`), {text: newSweet});
+        setEditing(false);
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewSweet(value);
+    };
+
+    return (
+        <div>
+            {editing ? (
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input type="text" placeholder="Edit your sweet" value={newSweet} required onChange={onChange} />
+                        <input type="submit" value="Update Sweet" />
+                    </form>
+                    <button onClick={toggleEditing}>Cancel</button>
+                </>
+            ) : (
+                <>
+                    <div>
+                        <h4>{sweetObj.text}</h4>
+                        {isOwner && (
+                            <>
+                            <button onClick={onDeleteClick}>Delete Sweet</button>
+                            <button onClick={toggleEditing}>Edit Sweet</button>
+                            </>
+                        )}
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Sweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { dbService } from "fBase";
 import { addDoc, collection, getDocs, onSnapshot, query, orderBy } from "firebase/firestore";
+import Sweet from "../components/Sweet";
 
 const Home = ({ userObj }) => {
     const [sweet, setSweet] = useState("");
     const [sweets, setSweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     
     useEffect(() => {
         const q = query(collection(dbService, 'sweets'));
@@ -33,23 +35,65 @@ const Home = ({ userObj }) => {
 
         setSweet("");
     };
+    /*const onSubmit = async (event) => {
+        event.preventDefault();
+
+        const attachmentRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+        const response = await uploadString(attachmentRef, attachment, "data_url");
+        const attachmentUrl = await getDownloadURL(response.ref);
+        const sweetObj = {
+            text: sweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
+            attachmentUrl
+        }
+
+        try {
+            const docRef = await addDoc(collection(dbService, "sweets"), {sweetObj});
+
+            console.log("Document written with ID: ", docRef.id);
+        } catch (error) {
+            console.log("Error adding document: ", error);
+        }
+
+        setSweet("");
+        setAttachment("");
+    };*/
     const onChange = (event) => {
         const {
             target: { value },
         } = event;
         setSweet(value);
     };
+    const onFileChange = (event) => {
+        const {
+            target: { files },
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget: { result },} = finishedEvent;
+            setAttachment(result)
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => setAttachment(null)
     return (
         <div>
             <form onSubmit={onSubmit}>
             <input value={sweet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120} />
+            <input type="file" accept="image/*" onChange={onFileChange} />
             <input type="submit" value="Sweet" />
+            {attachment && (
+                <div>
+                    <img src={attachment} width="50px" height="50px" />
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>
+            )}
         </form>
         <div>
             {sweets.map((sweet) => (
-                <div key={sweet.id}>
-                    <h4>{sweet.text}</h4>
-                </div>
+                <Sweet key={sweet.id} sweetObj={sweet} isOwner={sweet.creatorId === userObj.uid} />
             ))}
         </div>
     </div>
